9efd6e1ffb8569e29bbc8220849448e5443ce560
ellenfel <ellfelfen@gmail.com>
2023-03-07 12:29:04 +0300

    added script.py file

--- serverProject/Client/script.py
+++ serverProject/Client/script.py
@@ -1,10 +1,23 @@
+from time import sleep
 from opcua import Client
 from node import *
+
 client = Client("opc.tcp://ellenfel-Inspiron-3580:4840")
 client.connect()
-client.get_root_node()
-client.get_objects_node()
+rootNode = client.get_root_node()
+print("\nroot node =", rootNode)
+
+objectNode = client.get_objects_node()
+print("\nobject node =", objectNode)
+
+print("\nloading server hierarchie...\n")
+sleep(3)
+
 Server_Nodes = ServerNodes()
 Server_Nodes.build_list(Node(client.get_objects_node(), None))
 Server_Nodes.show_hierarchie()
-Server_Node.node_list[449].get_node_value()
+
+
+#Server_Node.node_list[449].get_node_value()
+#NameError
+
