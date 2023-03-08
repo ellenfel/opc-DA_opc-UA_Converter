b8cbce2361c97f42cc5d35cb60e7037c288e492f
ellenfel <ellfelfen@gmail.com>
2023-03-08 16:24:46 +0300

    done

--- serverProject/Client/myClient.c
+++ serverProject/Client/myClient.c
@@ -21,15 +21,62 @@ int main(void) {
     retval = UA_Client_readValueAttribute(client, nodeId, &value);
 
     if(retval == UA_STATUSCODE_GOOD &&
-       UA_Variant_hasScalarType(&value, &UA_TYPES[UA_TYPES_DATETIME])) {
+       UA_Variant_hasScalarType(&value, &UA_TYPES[UA_TYPES_DATETIME]))
+    {
         UA_DateTime raw_date = *(UA_DateTime *) value.data;
         UA_DateTimeStruct dts = UA_DateTime_toStruct(raw_date);
         UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "date is: %u-%u-%u %u:%u:%u.%03u\n",
                     dts.day, dts.month, dts.year, dts.hour, dts.min, dts.sec, dts.milliSec);
     }
 
+
+	//Variables for read access
+	UA_String VendorName;
+	UA_Int32 SerialNumber;
+	UA_Double Temperature;
+
+
+	//Read Vendor Name
+	retval = UA_Client_readValueAttribute(client,
+			UA_NODEID_STRING(2, "R1_TS1_VendorName"), &value);
+	if(retval == UA_STATUSCODE_GOOD &&
+			UA_Variant_hasScalarType(&value, &UA_TYPES[UA_TYPES_STRING]))
+	{   
+		VendorName =  *(UA_String *) value.data;
+		UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,
+				"The Vendor Name is %.*s", VendorName.length, VendorName.data);
+	}
+
+
+	//Read the Serial Number
+	retval = UA_Client_readValueAttribute(client, 
+			UA_NODEID_STRING(2, "R1_TS1_SerialNumber"), &value);
+	if(retval == UA_STATUSCODE_GOOD && 
+			UA_Variant_hasScalarType(&value, &UA_TYPES[UA_TYPES_INT32]))
+	{	
+		SerialNumber = *(UA_Int32 *) value.data;
+		UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,
+			       	"The Serial Number is %d", SerialNumber);
+	}
+
+
+	//Read the Temperature
+	retval = UA_Client_readValueAttribute(client,
+			UA_NODEID_STRING(2, "R1_TS1_Temperature"), &value);
+	if(retval == UA_STATUSCODE_GOOD &&
+			UA_Variant_hasScalarType(&value, &UA_TYPES[UA_TYPES_DOUBLE]))
+	{
+		Temperature = *(UA_Double *) value.data;
+		UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,
+				"The Temperature is %f", Temperature);
+	}
+
+
+
+
+
     /* Clean up */
-    UA_Variant_clear(&value);
+    //UA_Variant_clear(&value);
     UA_Client_delete(client); /* Disconnects the client internally */
     return EXIT_SUCCESS;
 }
